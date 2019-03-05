00acde07cf228dfe5f4bed110ea462c4f6f96b1f
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-05 12:32:24 +0100

    add handling str and Xpath type in adding operation,

--- xpath_string/xpath.py
+++ xpath_string/xpath.py
@@ -3,4 +3,10 @@ class Xpath:
         self.xpath = xpath_string
 
     def __add__(self, other):
-        pass
+        if isinstance(other, self.__class__):
+            return self.xpath + other.xpath
+        elif isinstance(other, str):
+            return self.xpath + other
+        else:
+            raise TypeError('Only Xpath and string can be added to Xpath Object. Not {}'.format(type(other)))
+
