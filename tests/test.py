eabdcadcfd95bfb66b487232a69bfb5754af65c0
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-05 12:32:52 +0100

    simple xpath adding and string add tests created

--- tests/test.py
+++ tests/test.py
@@ -6,11 +6,21 @@ from xpath_string.xpath import Xpath
 class TestXpath(unittest.TestCase):
     def setUp(self):
         self.object_1 = Xpath('//div')
+        self.object_2 = Xpath('//span')
 
     def test_object_creation(self):
-        assert type(self.object_1) is Xpath, \
+        assert isinstance(self.object_1, Xpath), \
             'object is not a Xpath class object. Is {} instead'.format(type(self.object_1))
 
     def test_object_arg_check(self):
         assert self.object_1.xpath == '//div', \
             'object xpath arg has value {} instead of {}'.format(self.object_1.xpath, '//div')
+
+    def test_string_add(self):
+        assert self.object_1 + 'Test', 'Addition of: "{}" and "Test" gives {} instead of {}'.format(
+            self.object_1, self.object_1 + 'Test', '//divTest')
+
+    def test_simple_xpath_add(self):
+        assert self.object_1 + self.object_2 == '//div//span', \
+            'Addition of: "{}" and "{}" gives {} instead of {}'.format(self.object_1, self.object_2,
+                                                                       self.object_1 + self.object_2, '//div//span')
