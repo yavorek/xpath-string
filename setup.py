3334c66d8945c88e6fd26d04a2bb8eb03fffc206
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-06 14:31:47 +0100

    created setup.py - v0.0.1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    version='0.0.1',
+    name='xpath_string',
+    description='Module which allows to make xpath operations over string which represent xpath',
+    author='Tomasz Jaworski',
+    author_email='jaworski.tomasz.91@gmail.com',
+    packages=find_packages()
+)
