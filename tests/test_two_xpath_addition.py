b4a968c4cc990b4319f83262e4dd8adfed59d828
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-06 14:17:18 +0100

    corrected test_str, test_two_xpath_addition added new cases with contains

--- tests/test_two_xpath_addition.py
+++ tests/test_two_xpath_addition.py
@@ -6,7 +6,21 @@ param_list_add_two_xpath_without_contains = [
     ('//span', '//div', '//span//div'),
     ('//div|//svg', '//span', '//div//span|//svg//span'),
     ('//div', '//span|//svg', '//div//span|//div//svg'),
-    ('//div|//span', '//avg|//span', '//div//avg|//span//avg|//div//span|//span//span')
+    ('//div|//span', '//avg|//span', '//div//avg|//span//avg|//div//span|//span//span'),
+    ('//div|//span[@class="abc"]//a|//a', '//avg[@name="ans"]|//avg/span/span', '//div//avg[@name="ans"]|//div//avg/span/span|//span[@class="abc"]//a//avg[@name="ans"]|//span[@class="abc"]//a//avg/span/span|//a//avg[@name="ans"]|//a//avg/span/span')
+]
+
+
+param_list_add_two_xpath_with_contains = [
+    ('//div[contains(a)]', '//a', '//div[contains(a)]//a'),
+    ('//zdf', '//avg[@class="a"]//div[contains(@abc, "base")]', '//zdf//avg[@class="a"]//div[contains(@abc, "base")]'),
+    ('//div[contains(a|b|c)]', '//a', '//div[contains(a|b|c)]//a'),
+    ('//link', '//span[@contains(d|@name, "blob")]', '//link//span[@contains(d|@name, "blob")]'),
+    ('//a[contains(b|c)]|//b', '/f', '//a[contains(b|c)]/f|//b/f'),
+    ('//a|//b', '/f[contains(b|c)]', '//a/f[contains(b|c)]|//b/f[contains(b|c)]'),
+    ('//a[contains(b|c|d)]', '//f|//avg/span', '//a[contains(b|c|d)]//f|//a[contains(b|c|d)]//avg/span'),
+    ('//a', '//f[contains(b|c|d)]|//avg/span[contains(b|c|d)]', '//a//f[contains(b|c|d)]|//a//avg/span[contains(b|c|d)]'),
+    ('//a[contains(b|c|d)]|//avg/span[contains(b|c|d)]', '//f', '//a[contains(b|c|d)]//f|//avg/span[contains(b|c|d)]//f')
 ]
 
 
@@ -14,6 +28,15 @@ class TestTwoXpathAddition(unittest.TestCase):
 
     def test_add_two_xpath_without_contains(self):
         for xpath_1, xpath_2, expected_result in param_list_add_two_xpath_without_contains:
+            with self.subTest():
+                set_result = set(TwoXpathAddition(xpath_1, xpath_2).add_two_xpath_together().split('|'))
+                set_expected_result = set(expected_result.split('|'))
+                assert set_result == set_expected_result, \
+                    'Addition of {} and {} is "{}" different than expected "{}"'.format(
+                        xpath_1, xpath_2, TwoXpathAddition(xpath_1, xpath_2).add_two_xpath_together(), expected_result)
+
+    def test_add_two_xpath_with_contains(self):
+        for xpath_1, xpath_2, expected_result in param_list_add_two_xpath_with_contains:
             with self.subTest():
                 set_result = set(TwoXpathAddition(xpath_1, xpath_2).add_two_xpath_together().split('|'))
                 set_expected_result = set(expected_result.split('|'))
--- tests/test_xpath.py
+++ tests/test_xpath.py
@@ -16,6 +16,10 @@ class TestXpath(unittest.TestCase):
         assert self.object_1.xpath == '//div', \
             'object xpath arg has value {} instead of {}'.format(self.object_1.xpath, '//div')
 
+    def test_str(self):
+        assert str(self.object_1) == '//div', \
+            '__str__ for Xpath is not working correctly. Received {} instead of {}'.format(str(self.object_1), '//div')
+
     def test_not_supported_type_add(self):
         try:
             self.object_1 + 123
