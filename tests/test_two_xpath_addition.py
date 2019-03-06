4d7ae04eb1dd6dccbfae09bcb499fd5e1f30119e
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-06 11:21:13 +0100

    changed way of checking xpath to use tuple instead of raw string - cannot ensure order of xpath parts in 'or' cases

--- tests/test_two_xpath_addition.py
+++ tests/test_two_xpath_addition.py
@@ -12,8 +12,10 @@ param_list_add_two_xpath_without_contains = [
 class TestTwoXpathAddition(unittest.TestCase):
 
     def test_add_two_xpath_without_contains(self):
-        for xpath_1, xpath_2, result in param_list_add_two_xpath_without_contains:
+        for xpath_1, xpath_2, expected_result in param_list_add_two_xpath_without_contains:
             with self.subTest():
-                assert TwoXpathAddition(xpath_1, xpath_2).add_two_xpath_together() == result, \
+                set_result = set(TwoXpathAddition(xpath_1, xpath_2).add_two_xpath_together().split('|'))
+                set_expected_result = set(expected_result.split('|'))
+                assert set_result == set_expected_result, \
                     'Addition of {} and {} is "{}" different than expected "{}"'.format(
-                        xpath_1, xpath_2, TwoXpathAddition(xpath_1, xpath_2).add_two_xpath_together(), result)
+                        xpath_1, xpath_2, TwoXpathAddition(xpath_1, xpath_2).add_two_xpath_together(), expected_result)
