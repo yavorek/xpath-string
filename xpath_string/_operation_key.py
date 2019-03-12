3cecf6742e83adf12f7cebc2259fc7d40752adbe
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-12 10:46:16 +0100

    change regex to cover all [] content not only 'contains' case

--- xpath_string/_operation_key.py
+++ xpath_string/_operation_key.py
@@ -2,7 +2,7 @@ import re
 
 
 class _OperationKey:
-    _or_operator_in_contains_part_regex = re.compile(r'contains\((((?!\)).)*)\)')
+    _or_operator_in_square_brackets_part_regex = re.compile(r'(?P<content>\[((?!\]).)*\])')
 
     def __init__(self, xpath_1: str, xpath_2: str):
         self.xpath_1 = xpath_1
@@ -23,9 +23,9 @@ class _OperationKey:
         return first_part + second_part
 
     def __or_operator_present_in_main_part_xpath_1(self):
-        xpath_without_contains_part = re.sub(self._or_operator_in_contains_part_regex, '', self.xpath_1)
+        xpath_without_contains_part = re.sub(self._or_operator_in_square_brackets_part_regex, '', self.xpath_1)
         return True if '|' in xpath_without_contains_part else False
 
     def __or_operator_present_in_main_part_xpath_2(self):
-        xpath_without_contains_part = re.sub(self._or_operator_in_contains_part_regex, '', self.xpath_2)
+        xpath_without_contains_part = re.sub(self._or_operator_in_square_brackets_part_regex, '', self.xpath_2)
         return True if '|' in xpath_without_contains_part else False
--- xpath_string/two_xpath_addition.py
+++ xpath_string/two_xpath_addition.py
@@ -23,43 +23,43 @@ class TwoXpathAddition(_OperationKey):
         split_xpath_1 = self._split_xpath_1()
         joined_xpath = '|'.join([part_of_xpath + self.xpath_2 for part_of_xpath in split_xpath_1])
         full_xpath = copy.deepcopy(joined_xpath)
-        for contains in self.xpath_1_contains_with_or_operator:
-            full_xpath = re.sub('CLEARED_1', 'contains({})'.format(contains), full_xpath, 1)
+        for contains in self.xpath_1_square_brackets_inside:
+            full_xpath = re.sub('CLEARED_1', '{}'.format(contains), full_xpath, 1)
         return full_xpath
 
     def add_xpath_with_or_operator(self):
         split_xpath_2 = self._split_xpath_2()
         joined_xpath = '|'.join([self.xpath_1 + part_of_xpath for part_of_xpath in split_xpath_2])
         full_xpath = copy.deepcopy(joined_xpath)
-        for contains in self.xpath_2_contains_with_or_operator:
-            full_xpath = re.sub('CLEARED_2', 'contains({})'.format(contains), full_xpath, 1)
+        for contains in self.xpath_2_square_brackets_inside:
+            full_xpath = re.sub('CLEARED_2', '{}'.format(contains), full_xpath, 1)
         return full_xpath
 
     def add_together_xpath_with_or_operator(self):
         split_xpath_1 = copy.deepcopy(self._split_xpath_1())
         split_xpath_2 = copy.deepcopy(self._split_xpath_2())
-        for contains_1 in self.xpath_1_contains_with_or_operator:
-            split_xpath_1 = re.sub('CLEARED_1', 'contains({})'.format(contains_1), ';!;'.join(split_xpath_1), 1).split(
+        for contains_1 in self.xpath_1_square_brackets_inside:
+            split_xpath_1 = re.sub('CLEARED_1', '{}'.format(contains_1), ';!;'.join(split_xpath_1), 1).split(
                 ';!;')
-        for contains_2 in self.xpath_2_contains_with_or_operator:
-            split_xpath_2 = re.sub('CLEARED_2', 'contains({})'.format(contains_2), ';!;'.join(split_xpath_2), 1).split(
+        for contains_2 in self.xpath_2_square_brackets_inside:
+            split_xpath_2 = re.sub('CLEARED_2', '{}'.format(contains_2), ';!;'.join(split_xpath_2), 1).split(
                 ';!;')
         split_xpath_product = [''.join(element) for element in itertools.product(split_xpath_1, split_xpath_2)]
         full_xpath = '|'.join(split_xpath_product)
         return full_xpath
 
     def _split_xpath_1(self):
-        self.xpath_1_contains_with_or_operator = [find[0] for find in
-                                                  re.findall(self._or_operator_in_contains_part_regex, self.xpath_1)]
-        xpath_1_without_contains_with_or_operator = re.sub(self._or_operator_in_contains_part_regex, 'CLEARED_1',
-                                                           self.xpath_1)
-        split_xpath = xpath_1_without_contains_with_or_operator.split('|')
+        self.xpath_1_square_brackets_inside = [find[0] for find in re.findall(
+            self._or_operator_in_square_brackets_part_regex, self.xpath_1)]
+        xpath_1_without_square_brackets = re.sub(
+            self._or_operator_in_square_brackets_part_regex, 'CLEARED_1', self.xpath_1)
+        split_xpath = xpath_1_without_square_brackets.split('|')
         return split_xpath
 
     def _split_xpath_2(self):
-        self.xpath_2_contains_with_or_operator = [find[0] for find in
-                                                  re.findall(self._or_operator_in_contains_part_regex, self.xpath_2)]
-        xpath_2_without_contains_with_or_operator = re.sub(self._or_operator_in_contains_part_regex, 'CLEARED_2',
-                                                           self.xpath_2)
-        split_xpath = xpath_2_without_contains_with_or_operator.split('|')
+        self.xpath_2_square_brackets_inside = [find[0] for find in re.findall(
+            self._or_operator_in_square_brackets_part_regex, self.xpath_2)]
+        xpath_2_without_square_brackets = re.sub(
+            self._or_operator_in_square_brackets_part_regex, 'CLEARED_2', self.xpath_2)
+        split_xpath = xpath_2_without_square_brackets.split('|')
         return split_xpath
