e2e0007b0b6efcf6d89f60b7b42488af79155beb
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-12 15:46:52 +0100

    version 0.1.5 -> 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='0.1.5',
+    version='0.2.0',
     name='xpath_string',
     description='Module which allows to make xpath operations over string which represent xpath',
     author='Tomasz Jaworski',
