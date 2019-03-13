8def52eece690a07930865c08dd82f062eaa63f2
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-13 12:34:21 +0100

    added long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     version='0.2.0',
     name='xpath_string',
     description='Module which allows to make xpath operations over string which represent xpath',
+    long_descritpion=long_description,
+    long_description_content_type='text/markdown',
     author='Tomasz Jaworski',
     author_email='jaworski.tomasz.91@gmail.com',
     packages=find_packages(),
