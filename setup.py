30c7c3ba4caebfb308b57340b068acfea9d8bd11
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-14 15:10:13 +0100

    changed long_description; remove type of it

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
-from os import path
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
 setup(
     version='0.2.11',
     name='xpath_string',
     description='Module which allows to make xpath operations over string which represent xpath',
-    long_descritpion=long_description,
-    long_description_content_type='text/markdown',
+    long_descritpion=readme,
     author='Tomasz Jaworski',
     author_email='jaworski.tomasz.91@gmail.com',
     packages=find_packages(),
