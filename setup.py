da991d84b62c65b4981141b60b2c238d5d343d2f
PLTOJAW1 <tomasz-x.jaworski@pl.abb.com>
2019-03-25 14:52:08 +0100

    0.2.13 -> 0.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 setup(
-    version='0.2.13',
+    version='0.2.2',
     name='xpath_string',
     description='Module which allows to make xpath operations over string which represent xpath',
     long_description=readme,
